50376d75ed2ac00e4bca3cb402362560a515452b
sahar <saharnajmi8089@gmail.com>
2024-06-26 16:29:58 +0200

    feature: add navigate

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
 }
 
 dependencies {
-
+    val nav_version = "2.7.7"
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -66,4 +66,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
+    implementation("androidx.navigation:navigation-compose:$nav_version")
 }
--- app/src/main/java/com/nextvocab/nextvocab/MainActivity.kt
+++ app/src/main/java/com/nextvocab/nextvocab/MainActivity.kt
@@ -3,14 +3,37 @@ package com.nextvocab.nextvocab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.nextvocab.nextvocab.ui.theme.NextVocabTheme
+import kotlinx.serialization.Serializable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +45,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    AppNavHost(navController = rememberNavController())
                 }
             }
         }
@@ -30,17 +53,111 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NextVocabTheme {
-        Greeting("Android")
+fun AppNavHost(
+    modifier: Modifier = Modifier,
+    navController: NavHostController,
+    startDestination: String = "ScreenA",
+) {
+    NavHost(
+        modifier = modifier,
+        navController = navController,
+        startDestination = startDestination
+    ) {
+        composable("ScreenA") {
+            ScreenA(navController)
+        }
+        composable("ScreenB") {
+            ScreenB(navController)
+        }
     }
 }
+
+@Composable
+fun ScreenA(navController: NavController) {
+    val dataList = listOf("test1", "test2", "test3")
+
+    Column(
+        modifier = Modifier.padding(24.dp),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.Start
+    ) {
+        Button(
+            onClick = {
+                navController.navigate("ScreenB")
+
+            }, colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF4CAF50),
+                contentColor = Color(0xFFFFFFFF)
+            ), modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Text("Add")
+        }
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 24.dp, end = 24.dp, top = 12.dp, bottom = 12.dp)
+                .weight(1f)
+        ) {
+            items(dataList) { item ->
+                Text(
+                    text = item,
+                )
+            }
+        }
+        Button(
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFFFF5722),
+                contentColor = Color(0xFFFFFFFF)
+            ),
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Text(text = "Study")
+        }
+    }
+
+}
+
+@Composable
+fun ScreenB(navController: NavController) {
+    var text by remember { mutableStateOf("") }
+    val itemList = remember { mutableStateListOf<String>() }
+    Column(
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Row(
+            modifier = Modifier.padding(24.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            TextField(
+                value = text,
+                onValueChange = { newText ->
+                    text = newText
+                }
+            )
+            Button(
+                onClick = {
+                    itemList.add(text)
+                    navController.navigate("ScreenA")
+
+                }, colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF4CAF50),
+                    contentColor = Color(0xFFFFFFFF)
+                )
+            ) {
+                Text("Add")
+            }
+        }
+
+    }
+
+
+
+    @Serializable
+    data class AddNewWord(val name: String)
+}
+
+@Serializable
+data class AddNewWord(val name: String)
